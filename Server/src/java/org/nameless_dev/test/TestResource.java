70ebc9f5f39ebd6e1ef8061b0a4127665c26a695
MinerMax555 <minermax555@gmail.com>
2018-08-09 18:03:59 +0200

    Merge App and Server Projects

---
+++ Server/src/java/org/nameless_dev/test/TestResource.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.nameless_dev.test;
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * REST Web Service
+ *
+ * @author Admin
+ */
+@Path("Test")
+public class TestResource {
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of TestResource
+     */
+    public TestResource() {
+    }
+
+    /**
+     * Retrieves representation of an instance of org.nameless_dev.test.TestResource
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_XML)
+    public String getXml() {
+        //TODO return proper representation object
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * PUT method for updating or creating an instance of TestResource
+     * @param content representation for the resource
+     */
+    @PUT
+    @Consumes(MediaType.APPLICATION_XML)
+    public void putXml(String content) {
+    }
+}
