0ceead40b7ece18440b6a4edf94cbad03492cd27
Unknown <max.walder@hotmail.de>
2018-08-18 15:48:39 +0200

    Implemented XmlSerializable
    added an interface to unify Xstream defenitions

---
+++ Server/src/java/org/namedev/XmlSerializable.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.namedev;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.io.xml.StaxDriver;
+
+import org.namedev.test.TestUserEntity;
+
+/**
+ * An Interface that all Entity classes that are supposed to be transferred between Client and Server
+ * must implement.
+ * @author Max
+ */
+public interface XmlSerializable {
+    /**
+     * Creates a new instance of {@code XStream} and initializes it with
+     * the common aliases and attributes used within this project.
+     * 
+     * @return the XStream instance
+     */
+    public static XStream getXmlStream(){
+        XStream stream = new XStream(new StaxDriver());
+        
+        stream.alias("test_user", TestUserEntity.class);
+        
+        //variables which are saved as attributes instead of fields
+        stream.useAttributeFor(TestUserEntity.class, "id");
+        
+        return stream;
+    }
+}
