4a31d85ecd657eb970b81ca2c48ba589bd9df0e8
Unknown <max.walder@hotmail.de>
2018-08-18 15:50:51 +0200

    Removed unnecessary code from TestUserResource
    starting to create classes that actually interact with the database, thus removing the old things

---
+++ Server/src/java/org/namedev/test/TestUserResource.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.namedev.test;
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import lib.db.controller.Controller;
+
+/**
+ * REST Web Service
+ *
+ * @author Max
+ */
+@Path("/Test")
+public class TestUserResource {
+
+    private final Controller<TestUserEntity> controller;
+    
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of TestResource
+     */
+    public TestUserResource() {
+        controller = new Controller<>("MDB_PU", TestUserEntity.class);
+    }
+    
+    
+}
