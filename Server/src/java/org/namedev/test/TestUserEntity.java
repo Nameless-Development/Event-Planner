fe36854bff41dfa2165a388d27f73ea9e8d6440d
Unknown <max.walder@hotmail.de>
2018-08-25 14:35:53 +0200

    Added an update and a delete function to TestUser
    also, formatting

--- Server/src/java/org/namedev/test/TestUserEntity.java
+++ Server/src/java/org/namedev/test/TestUserEntity.java
@@ -22,61 +22,60 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "tbl_user")
 @NamedQueries({
-    @NamedQuery(name = "TestUserEntity.findbyId", query = "SELECT tu FROM TestUserEntity tu WHERE tu.id LIKE :id")
+  @NamedQuery(name = "TestUserEntity.findbyId", query = "SELECT tu FROM TestUserEntity tu WHERE tu.id LIKE :id"),
+  @NamedQuery(name = "TestUserEntity.update", query = "UPDATE TestUserEntity tu SET tu.username = :username WHERE tu.id LIKE :id")
 })
 public class TestUserEntity implements Serializable {
 
-    private static final long serialVersionUID = 1L;
-    
-    
-    @Id
-    @Column(name = "id")
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-    
-    @Column(name = "username")
-    private String username;
+  private static final long serialVersionUID = 1L;
 
+  @Id
+  @Column(name = "id")
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
 
-    public Long getId() {
-        return id;
-    }
+  @Column(name = "username")
+  private String username;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+  public Long getId() {
+    return id;
+  }
 
-    public String getUsername() {
-        return username;
-    }
-    
-    public void setUsername(String username) {
-        this.username = username;
-    }
-    
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
-    }
+  public void setId(Long id) {
+    this.id = id;
+  }
 
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof TestUserEntity)) {
-            return false;
-        }
-        TestUserEntity other = (TestUserEntity) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
-    }
+  public String getUsername() {
+    return username;
+  }
 
-    @Override
-    public String toString() {
-        return "TestUserEntity[ id=" + id + " ]";
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  @Override
+  public int hashCode() {
+    int hash = 0;
+    hash += (id != null ? id.hashCode() : 0);
+    return hash;
+  }
+
+  @Override
+  public boolean equals(Object object) {
+    // TODO: Warning - this method won't work in the case the id fields are not set
+    if (!(object instanceof TestUserEntity)) {
+      return false;
     }
-    
+    TestUserEntity other = (TestUserEntity) object;
+    if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+      return false;
+    }
+    return true;
+  }
+
+  @Override
+  public String toString() {
+    return "TestUserEntity[ id=" + id + " ]";
+  }
+
 }
